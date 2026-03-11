1d1cd778f521d00ddc005c10837a88326baac1c3
Gustavo Amaro <gustavo.amaro97@gmail.com>
2026-03-11 13:31:35 -0300

    discart

--- migrations/Version20260216205145.php
+++ migrations/Version20260216205145.php
@@ -20,8 +20,8 @@ final class Version20260216205145 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE shop_schedule (id INT AUTO_INCREMENT NOT NULL, day_of_week SMALLINT UNSIGNED NOT NULL, is_open TINYINT DEFAULT 1 NOT NULL, time_open TIME DEFAULT NULL, time_close TIME DEFAULT NULL, shop_id INT NOT NULL, INDEX IDX_8180EB6D4D16C4DD (shop_id), UNIQUE INDEX shop_day_unique (shop_id, day_of_week), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE shop_schedule ADD CONSTRAINT FK_8180EB6D4D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE CASCADE');
+        //$this->addSql('CREATE TABLE shop_schedule (id INT AUTO_INCREMENT NOT NULL, day_of_week SMALLINT UNSIGNED NOT NULL, is_open TINYINT DEFAULT 1 NOT NULL, time_open TIME DEFAULT NULL, time_close TIME DEFAULT NULL, shop_id INT NOT NULL, INDEX IDX_8180EB6D4D16C4DD (shop_id), UNIQUE INDEX shop_day_unique (shop_id, day_of_week), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        //$this->addSql('ALTER TABLE shop_schedule ADD CONSTRAINT FK_8180EB6D4D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
